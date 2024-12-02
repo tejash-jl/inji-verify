7c034c0ac2a2cc4ea8209fdb9aecd7274e876d90
Kumar M <pkumarm@deloitte.com>
2024-12-02 16:46:33 +0530

    added logo

--- inji-verify/src/components/Home/VerificationProgressTracker/Navbar.tsx
+++ inji-verify/src/components/Home/VerificationProgressTracker/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar(props: any) {
     // Logo goes here
     return (
         <NavbarContainer>
-            <img src='/assets/images/inji_verify.svg'/>
+            <img src="https://i.ibb.co/7vdwCWL/Inji-Web-Logo.png" style={{height:'70px'}}/>
         </NavbarContainer>
     );
 }
